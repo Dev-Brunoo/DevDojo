6a61e1a2d89040f61fcccd787e4ecb0de7ef9384
Bruno tadeu <brunoo.nat4li@gmail.com>
2026-03-24 19:25:10 -0300

    Aula 01,02 e 03 aprendendo sobre alguns conceitos basicos da linguagem Java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+
+    static void main(String[] args) {
+        // int, double, float, chat, byte, short, long, boolean
+
+        int age = (int) 10000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        String nome = "Bruno";
+        System.out.println("The age is:"+age+"anos");
+        System.out.println(nome);
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExerciciojava.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExerciciojava {
+    public static void main(String[]args){
+        String nomeFunc = "Bruno";
+        String enderecoFunc = "Rua Eugenio leite da cruz" ;
+        double salarioDouble = 2500.00;
+        String dataRecebimentoSalario = "20/09/2020";
+        String relatorio = "Eu " +nomeFunc+ ", morando no endereço " +enderecoFunc+ ", confirmo que recebi o salário de " +salarioDouble+ ", na data " +dataRecebimentoSalario;
+
+        System.out.println( relatorio);
+    }
+}
